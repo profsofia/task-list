4dbf55653e4d7e1662df612a76709e3632facdad
Sofía Schenone <sofiainesschenone@gmail.com>
2021-12-16 14:00:02 -0300

    Tenemos la lista de tareas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BotonTasksComponent } from './components/boton-tasks/boton-tasks.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BotonTasksComponent
+    BotonTasksComponent,
+    TasksComponent,
+    TasksItemComponent
   ],
   imports: [
     BrowserModule,
